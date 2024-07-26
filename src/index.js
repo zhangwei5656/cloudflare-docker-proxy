451aae6e86be561544fd8e10620a80bca2e4c331
ZHANG Wei <zhangwedu@mm.lvtu.in>
2024-07-26 09:21:29 +0800

    change routes

--- src/index.js
+++ src/index.js
@@ -7,14 +7,8 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "dockerhub.tune.us.kg": dockerHub,
+  "gcr.tune.us.kg": "https://gcr.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
